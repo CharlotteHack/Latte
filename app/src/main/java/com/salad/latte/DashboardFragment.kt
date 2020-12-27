414370588cdc1ed5c44fd592e357ba2e49f331ea
Mohamed <kukgiplayer2@gmail.com>
2020-12-26 19:13:23 -0500

    updates

--- app/src/main/java/com/salad/latte/DashboardFragment.kt
+++ app/src/main/java/com/salad/latte/DashboardFragment.kt
@@ -10,12 +10,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
-import com.github.mikephil.charting.utils.Utils
+import org.eazegraph.lib.charts.BarChart
+import org.eazegraph.lib.models.BarModel
 
 class DashboardFragment : Fragment(){
 
@@ -42,51 +38,18 @@ class DashboardFragment : Fragment(){
     }
 
 
-    fun initChart(view :View, context : Context){
-        var mChart = view.findViewById(R.id.chart_month_by_month) as LineChart;
-        mChart.setTouchEnabled(true);
-        mChart.setPinchZoom(true);
-        val values: ArrayList<Entry> = ArrayList()
-        values.add(Entry(1f, 50f))
-        values.add(Entry(1f, 50f))
+    fun initChart(view :View, context : Context) {
+        val mBarChart = view.findViewById(R.id.chart_month_by_month) as BarChart
 
-//        Display the data
-        val set1: LineDataSet
-        if (mChart.data != null &&
-            mChart.data.dataSetCount > 0
-        ) {
-            set1 = mChart.data.getDataSetByIndex(0) as LineDataSet
-            set1.values = values
-            mChart.data.notifyDataChanged()
-            mChart.notifyDataSetChanged()
-        } else {
-            set1 = LineDataSet(values, "Sample Data")
-            set1.setDrawIcons(false)
-            set1.enableDashedLine(10f, 5f, 0f)
-            set1.enableDashedHighlightLine(10f, 5f, 0f)
-            set1.color = Color.DKGRAY
-            set1.setCircleColor(Color.DKGRAY)
-            set1.lineWidth = 1f
-            set1.circleRadius = 3f
-            set1.setDrawCircleHole(false)
-            set1.valueTextSize = 9f
-            set1.setDrawFilled(true)
-            set1.formLineWidth = 1f
-            set1.formLineDashEffect = DashPathEffect(floatArrayOf(10f, 5f), 0f)
-            set1.formSize = 15f
-            if (Utils.getSDKInt() >= 18) {
-                val drawable = ContextCompat.getDrawable(context, R.drawable.ic_launcher_background)
-                set1.fillDrawable = drawable
-            } else {
-                set1.fillColor = Color.DKGRAY
-            }
-            val dataSets: ArrayList<ILineDataSet> = ArrayList()
-            dataSets.add(set1)
-            val data = LineData(dataSets)
-            mChart.data = data
-        }
+        mBarChart.addBar(BarModel(-2.3f, -0xedcbaa))
+        mBarChart.addBar(BarModel(2f, -0xcbcbaa))
+        mBarChart.addBar(BarModel(3.3f, -0xa9cbaa))
+        mBarChart.addBar(BarModel(1.1f, -0x78c0aa))
+        mBarChart.addBar(BarModel(2.7f, -0xa9480f))
+        mBarChart.addBar(BarModel(2f, -0xcbcbaa))
+        mBarChart.addBar(BarModel(0.4f, -0xe00b54))
+        mBarChart.addBar(BarModel(4f, -0xe45b1a))
 
-//        Cumulative Chart
-        var cumChart = view.findViewById(R.id.chart_cumulative) as LineChart
+        mBarChart.startAnimation()
     }
 }
--- app/src/main/java/com/salad/latte/MainFragment.kt
+++ app/src/main/java/com/salad/latte/MainFragment.kt
@@ -1,20 +1,14 @@
 package com.salad.latte
 
+import android.R
 import android.content.Context
-import android.graphics.Color
-import android.graphics.DashPathEffect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.github.mikephil.charting.charts.BarChart
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.data.*
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
-import com.github.mikephil.charting.utils.Utils
+import org.eazegraph.lib.charts.BarChart
+import org.eazegraph.lib.models.BarModel
 
 
 class MainFragment() : Fragment() {
@@ -24,53 +18,13 @@ class MainFragment() : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var view = inflater.inflate(R.layout.fragment_main,container,false);
-        initChart(view, activity!!.application)
+        //var view = inflater.inflate(R.layout.fragment_main,container,false);
+        //initChart(view, activity!!.application)
         return view;
     }
 
     fun initChart(view :View, context :Context){
-        var mChart = view.findViewById(R.id.chart_month_by_month) as BarChart;
-        mChart.setTouchEnabled(true);
-        mChart.setPinchZoom(true);
-        val values: ArrayList<BarEntry> = ArrayList()
-        values.add(BarEntry(1.1f, 50f))
-//        Display the data
-        val set1: BarDataSet
-        if (mChart.data != null &&
-            mChart.data.dataSetCount > 0
-        ) {
-            set1 = mChart.data.getDataSetByIndex(0) as BarDataSet
-            set1.values = values
-            mChart.data.notifyDataChanged()
-            mChart.notifyDataSetChanged()
-        } else {
-            set1 = BarDataSet(values, "Monthly Return")
-            set1.setDrawIcons(false)
-            set1.label = "okur"
-            //set1.enableDashedLine(10f, 5f, 0f)
-            //set1.enableDashedHighlightLine(10f, 5f, 0f)
-            set1.color = Color.DKGRAY
-            //set1.setCircleColor(Color.DKGRAY)
-            //set1.lineWidth = 1f
-            //set1.circleRadius = 3f
-            //set1.setDrawCircleHole(false)
-            set1.valueTextSize = 9f
-            //set1.setDrawFilled(true)
-            set1.formLineWidth = 1f
-            set1.formLineDashEffect = DashPathEffect(floatArrayOf(10f, 5f), 0f)
-            set1.formSize = 15f
-            if (Utils.getSDKInt() >= 18) {
-                val drawable = ContextCompat.getDrawable(context, R.drawable.ic_launcher_background)
-                //set1.fillDrawable = drawable
-            } else {
-                //set1.fillColor = Color.DKGRAY
-            }
-            val dataSets: ArrayList<IBarDataSet> = ArrayList()
-            dataSets.add(set1)
-            val data = BarData(dataSets)
-            data.dataSetLabels.set(0,"ok")
-            mChart.data = data
-        }
+
+
     }
 }
