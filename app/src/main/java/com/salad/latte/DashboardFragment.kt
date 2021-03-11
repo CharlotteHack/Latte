b35427ce7ee56bf04e1affad65e242f51dcef05b
Mohamed <kukgiplayer2@gmail.com>
2021-03-10 23:53:54 -0500

    watchlist complete

--- app/src/main/java/com/salad/latte/DashboardFragment.kt
+++ app/src/main/java/com/salad/latte/DashboardFragment.kt
@@ -1,25 +1,16 @@
 package com.salad.latte
 
-import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.GridView
-import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.jjoe64.graphview.GraphView
-import com.jjoe64.graphview.series.BarGraphSeries
-import com.jjoe64.graphview.series.DataPoint
-import com.jjoe64.graphview.series.LineGraphSeries
-import com.salad.latte.Adapters.NewsAdapter
 import com.salad.latte.Adapters.WatchListAdapter
-import com.salad.latte.Objects.News
+import com.salad.latte.Database.FirebaseDB
 import com.salad.latte.Objects.Watchlist
-import org.eazegraph.lib.charts.BarChart
-import org.eazegraph.lib.models.BarModel
-import java.util.*
 
 
 class DashboardFragment : Fragment(){
@@ -33,11 +24,16 @@ class DashboardFragment : Fragment(){
     lateinit var watchitems :ArrayList<Watchlist>
     lateinit var watchAdapter :WatchListAdapter
 
+    lateinit var firebaseDB :FirebaseDB
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        firebaseDB = FirebaseDB();
+        watchitems = ArrayList()
         val view = inflater.inflate(R.layout.fragment_dashboard,container,false)
 //        tv_totaltrades = view.findViewById(R.id.tv_totaltrades2)
 //        tv_winrate = view.findViewById(R.id.tv_winrate2)
@@ -46,29 +42,32 @@ class DashboardFragment : Fragment(){
 
 
         gv_watchlist = view.findViewById(R.id.gv_watchlist)
-        watchitems = ArrayList<Watchlist>()
-        var watchlistItem = Watchlist();
-        watchlistItem.icon = "https://c0.klipartz.com/pngpicture/203/134/gratis-png-visa.png"
-        watchlistItem.ticker = "VISA (V)"
-        watchlistItem.allocation = "Allocation: 2%"
-        watchlistItem.targetEntry = "Target Entry: $100.71"
-        watchlistItem.buySellOrHold = "HOLD"
-        watchlistItem.entryDate = "12/31"
-        watchitems.add(Watchlist("https://e7.pngegg.com/pngimages/910/492/png-clipart-mastercard-logo-credit-card-visa-brand-mastercard-text-label.png","MA","Target Entry: $336.00",
-        "Allocation: 2%","HOLD","1/1"));
+        watchitems.addAll(firebaseDB.pullWatchlistData(context!!,R.layout.custom_news_item,gv_watchlist))
+//        var watchlistItem = Watchlist();
+//        watchlistItem.icon = "https://c0.klipartz.com/pngpicture/203/134/gratis-png-visa.png"
+//        watchlistItem.ticker = "VISA (V)"
+//        watchlistItem.allocation = "Allocation: 2%"
+//        watchlistItem.targetEntry = "Target Entry: $100.71"
+//        watchlistItem.buySellOrHold = "HOLD"
+//        watchlistItem.entryDate = "12/31"
+//        watchitems.add(Watchlist("https://e7.pngegg.com/pngimages/910/492/png-clipart-mastercard-logo-credit-card-visa-brand-mastercard-text-label.png","MA","Target Entry: $336.00",
+//        "Allocation: 2%","HOLD","1/1"));
 
 
 
 
-        watchitems.add(watchlistItem)
+//        watchitems.add(watchlistItem)
 //        watchitems.add(Watchlist())
 //        watchitems.add(Watchlist())
 //        watchitems.add(Watchlist())
 //        watchitems.add(Watchlist())
 //        watchitems.add(Watchlist())//
+
+        Log.d("FirebaseDB2", "Results found for watchlist: " + watchitems.size)
         watchAdapter = WatchListAdapter(context!!,R.layout.custom_news_item,watchitems)
         gv_watchlist.setAdapter(watchAdapter)
         watchAdapter.notifyDataSetChanged()
+
 
 
 
--- app/src/main/java/com/salad/latte/Database/FirebaseDB.java
+++ app/src/main/java/com/salad/latte/Database/FirebaseDB.java
@@ -1,9 +1,70 @@
 package com.salad.latte.Database;
 
+import android.content.Context;
+import android.util.Log;
+import android.widget.GridView;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.salad.latte.Adapters.WatchListAdapter;
+import com.salad.latte.Objects.Watchlist;
+import com.salad.latte.R;
+
+import java.io.Console;
+import java.util.ArrayList;
+
 public class FirebaseDB {
-
+    private final DatabaseReference mDatabase;
+    ValueEventListener watchlistReference;
+    ArrayList<Watchlist> watchlistItems;
+    WatchListAdapter watchListAdapter;
     public FirebaseDB(){
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        watchlistItems = new ArrayList();
 
+    }
 
+    public ArrayList<Watchlist> pullWatchlistData(Context context, int layout, GridView gridView){
+
+        if (watchlistReference != null){
+            mDatabase.removeEventListener(watchlistReference);
+        }
+       watchlistReference = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                watchlistItems.clear();
+
+                Log.d("FirebaseDB","Snapshot count: "+snapshot.child("dashboard").child("watchlist").getChildrenCount());
+                for(DataSnapshot datasnap: snapshot.child("dashboard").child("watchlist").getChildren()){
+                    watchlistItems.add(
+                            new Watchlist(
+                                    datasnap.child("icon").getValue(String.class),
+                                    datasnap.child("ticker").getValue(String.class),
+                                    datasnap.child("targetEntry").getValue(String.class),
+                                    datasnap.child("allocation").getValue(String.class),
+                                    datasnap.child("buySellHold").getValue(String.class),
+                                    datasnap.child("entryDate").getValue(String.class)
+                            )
+                    );
+                }
+                //
+                watchListAdapter = new WatchListAdapter(context,layout,watchlistItems);
+                gridView.setAdapter(watchListAdapter);
+                watchListAdapter.notifyDataSetChanged();
+       }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        mDatabase.addValueEventListener(watchlistReference);
+        Log.d("FirebaseDB", "Results found for watchlist: " + watchlistItems.size());
+        return watchlistItems;
     }
 }
