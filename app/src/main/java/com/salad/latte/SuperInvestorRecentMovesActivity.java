99109fa29acd396355a278534837b075624c90ce
Mohamed <kukgiplayer2@gmail.com>
2021-09-23 20:57:13 -0400

    SuperInvestorAdapter
    Bug fixes for FirebaseDB
    Turned off historical activity
    Moved recent moves to an activity style instead of fragment
    Moved dividends to an activity style instead of fragment
    Made the super investor buttons invisible
    Designed UI for Super investor Recent moves
    Added logic to allow user to view recent moves activity from super investors
    Fixed issue with super investor return percent (for some reason we have to divide the holdings size by 3 in superinvest..)
    Build 5 complete
    Updated super investors positions list
    Fixed issue with FB being called 3 times
    Added logic for positions
    Removed Toolbar

--- app/src/main/java/com/salad/latte/SuperInvestorRecentMovesActivity.java
+++ app/src/main/java/com/salad/latte/SuperInvestorRecentMovesActivity.java
@@ -1,6 +1,8 @@
 package com.salad.latte;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -10,6 +12,7 @@ public class SuperInvestorRecentMovesActivity extends AppCompatActivity {
 
     TextView superInvestorName;
     TextView ytdPerformance;
+    View recentMovesBtn;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +23,13 @@ public class SuperInvestorRecentMovesActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         superInvestorName = findViewById(R.id.tv_recents_investor);
         ytdPerformance = findViewById(R.id.tv_recents_ytd);
+        recentMovesBtn = findViewById(R.id.btn_recentmoves_finish);
+        recentMovesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
         superInvestorName.setText(bundle.getString("superInvestor"));
         ytdPerformance.setText(bundle.getString("ytd")+"% Return to date");
 
