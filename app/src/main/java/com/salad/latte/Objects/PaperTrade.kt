0008298bfeb2a0b4b7e9de71623efa0f38e7be9c
Mohamed <kukgiplayer2@gmail.com>
2022-03-14 18:13:36 -0400

    added paper

---
+++ app/src/main/java/com/salad/latte/Objects/PaperTrade.kt
@@ -0,0 +1,12 @@
+package com.salad.latte.Objects
+
+class PaperTrade {
+    lateinit var timestamp :String
+    var entry = 0.0
+    lateinit var entryTimestamp :String
+    lateinit var exitTimestamp :String
+    var exit = 0.0
+    var returnPercent = 0.0
+
+
+}
