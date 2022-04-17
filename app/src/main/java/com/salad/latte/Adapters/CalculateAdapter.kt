670c42be2aba05c53b92bd4ecbdc6971e45df7d4
mohamedsalad <kukgiplayer2@gmail.com>
2022-04-17 15:59:43 -0400

    added logic for calculate adapter

--- app/src/main/java/com/salad/latte/Adapters/CalculateAdapter.kt
+++ app/src/main/java/com/salad/latte/Adapters/CalculateAdapter.kt
@@ -1,4 +1,45 @@
 package com.salad.latte.Adapters
 
-class CalculateAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.salad.latte.Objects.CalculateItem
+import com.salad.latte.R
+import com.squareup.picasso.Picasso
+
+class CalculateAdapter(var calculateItems :ArrayList<CalculateItem>, var context : Context) : RecyclerView.Adapter<CalculateViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalculateViewHolder {
+        val inflater = LayoutInflater.from(context).inflate(R.layout.custom_calculate_item,parent,false)
+        return CalculateViewHolder(inflater)
+    }
+
+    override fun onBindViewHolder(holder: CalculateViewHolder, position: Int) {
+        Picasso.get().load(calculateItems.get(position).imgUrl).into(holder.imgView)
+        holder.ticker_tv.setText(calculateItems.get(position).ticker)
+        holder.entryPrice_tv.setText(calculateItems.get(position).entryPrice.toString())
+        if(calculateItems.get(position).totalCapital == 0f){
+            holder.sharesToBuy_tv.setText("--")
+        }
+        else{
+            var adjustedCapitalAfterAllocation = calculateItems.get(position).totalCapital*calculateItems.get(position).allocation
+            var sharesToBuy = Integer.parseInt((adjustedCapitalAfterAllocation/calculateItems.get(position).entryPrice).toString())
+            holder.sharesToBuy_tv.setText(sharesToBuy.toString())
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return calculateItems.size
+    }
+}
+
+class CalculateViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+    lateinit var imgView :ImageView
+    lateinit var ticker_tv :TextView
+    lateinit var entryPrice_tv :TextView
+    lateinit var sharesToBuy_tv :TextView
+
 }
--- app/src/main/java/com/salad/latte/Objects/CalculateItem.kt
+++ app/src/main/java/com/salad/latte/Objects/CalculateItem.kt
@@ -1,4 +1,10 @@
 package com.salad.latte.Objects
 
-class CalculateItem {
+class CalculateItem (tickImg :String, tick :String, ep :Float, stb :Float, totCap :Float, alloc :Float) {
+    var imgUrl = tickImg
+    var ticker = tick
+    var entryPrice = ep
+    var sharesToBuy = stb
+    var totalCapital =  totCap
+    var allocation = alloc
 }
