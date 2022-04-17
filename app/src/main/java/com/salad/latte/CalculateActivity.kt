c0599216b1d793273074e4c222b4ad5e9da4292c
mohamedsalad <kukgiplayer2@gmail.com>
2022-04-17 18:06:19 -0400

    connected calculator and rv

--- app/src/main/java/com/salad/latte/CalculateActivity.kt
+++ app/src/main/java/com/salad/latte/CalculateActivity.kt
@@ -1,11 +1,15 @@
 package com.salad.latte
 
 import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.salad.latte.Adapters.CalculateAdapter
 import com.salad.latte.Objects.CalculateItem
@@ -14,45 +18,44 @@ import java.lang.Exception
 
 class CalculateActivity : Activity() {
 
-    lateinit var calculate_et :EditText
-    lateinit var calculate_btn :Button
-    lateinit var calculate_rv :RecyclerView
-    lateinit var calculateAdapter :CalculateAdapter
-    lateinit var calculateItems :ArrayList<CalculateItem>
+    lateinit var calculate_et: EditText
+    lateinit var calculate_btn: Button
+    lateinit var calculate_rv: RecyclerView
+    lateinit var calculateAdapter: CalculateAdapter
+    lateinit var calculateItems: ArrayList<CalculateItem>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculate_daily_investment)
 //        Log.d("CalculateActivity", "Boop")
 
-        if(intent.hasExtra("picks")){
+        if (intent.hasExtra("picks")) {
 //            var items = intent.getStringArrayExtra("items")
-    try {
+            try {
 //        val items = intent.getParcelableArrayExtra("picks")
 //        Log.d("CalculateActivity", "Picks found: " + items!!.size)
-        calculate_btn = findViewById(R.id.calculate_button)
-        calculate_rv = findViewById(R.id.calculate_list_rv)
-        calculate_et = findViewById(R.id.calculate_invest_et)
-        calculateItems = intent.getParcelableArrayExtra("picks") as ArrayList<CalculateItem>
+                calculate_btn = findViewById(R.id.calculate_button)
+                calculate_rv = findViewById(R.id.calculate_list_rv)
+                calculate_et = findViewById(R.id.calculate_invest_et)
+                calculateItems = intent.getParcelableArrayExtra("picks") as ArrayList<CalculateItem>
                 Log.d("CalculateActivity", "Picks found: " + calculateItems.size)
 
-        calculateAdapter = CalculateAdapter(calculateItems,this)
-        calculate_rv.adapter = calculateAdapter
-        calculateAdapter.notifyDataSetChanged()
-        calculate_btn.setOnClickListener {
-            var investmentVal = calculate_et.text.toString()
-            if(isNumber(investmentVal)){
-                updateListWithNewCapital(investmentVal)
-            }
-            else{
-                Toast.makeText(this,"Not a valid number",Toast.LENGTH_LONG).show()
-            }
-        }
+                calculateAdapter = CalculateAdapter(calculateItems, this)
+                calculate_rv.adapter = calculateAdapter
+                calculateAdapter.notifyDataSetChanged()
+                calculate_btn.setOnClickListener {
+                    var investmentVal = calculate_et.text.toString()
+                    if (isNumber(investmentVal)) {
+                        updateListWithNewCapital(investmentVal)
+                    } else {
+                        Toast.makeText(this, "Not a valid number", Toast.LENGTH_LONG).show()
+                    }
+                    hideKeyboard()
+                }
 
-    }
-    catch (e : Exception){
-        e.printStackTrace()
-    }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
 
 
         }
@@ -63,10 +66,24 @@ class CalculateActivity : Activity() {
         return if (s.isNullOrEmpty()) false else s.all { Character.isDigit(it) }
     }
 
-    fun updateListWithNewCapital(investCap :String){
+    fun updateListWithNewCapital(investCap: String) {
         calculateItems.forEach({
             it.totalCapital = investCap as Float
         })
         calculateAdapter.notifyDataSetChanged()
     }
+
+    fun Fragment.hideKeyboard() {
+        view?.let { activity?.hideKeyboard(it) }
+    }
+
+    fun Activity.hideKeyboard() {
+        hideKeyboard(currentFocus ?: View(this))
+    }
+
+    fun Context.hideKeyboard(view: View) {
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+
+    }
+
 }
