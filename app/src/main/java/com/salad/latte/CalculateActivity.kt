dd0c6c5096ba88ac27ef658333ab1ce8dd6a1b52
mohamedsalad <kukgiplayer2@gmail.com>
2022-04-17 16:08:13 -0400

    connected calculator and rv

--- app/src/main/java/com/salad/latte/CalculateActivity.kt
+++ app/src/main/java/com/salad/latte/CalculateActivity.kt
@@ -7,6 +7,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.salad.latte.Adapters.CalculateAdapter
+import com.salad.latte.Objects.CalculateItem
 import java.lang.Exception
 
 
@@ -15,35 +17,43 @@ class CalculateActivity : Activity() {
     lateinit var calculate_et :EditText
     lateinit var calculate_btn :Button
     lateinit var calculate_rv :RecyclerView
+    lateinit var calculateAdapter :CalculateAdapter
+    lateinit var calculateItems :ArrayList<CalculateItem>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculate_daily_investment)
-        Log.d("CalculateActivity", "Boop")
+//        Log.d("CalculateActivity", "Boop")
 
         if(intent.hasExtra("picks")){
 //            var items = intent.getStringArrayExtra("items")
     try {
-        val items = intent.getParcelableArrayExtra("picks")
-        Log.d("CalculateActivity", "Picks found: " + items!!.size)
+//        val items = intent.getParcelableArrayExtra("picks")
+//        Log.d("CalculateActivity", "Picks found: " + items!!.size)
+        calculate_btn = findViewById(R.id.calculate_button)
+        calculate_rv = findViewById(R.id.calculate_list_rv)
+        calculate_et = findViewById(R.id.calculate_invest_et)
+        calculateItems = intent.getParcelableArrayExtra("picks") as ArrayList<CalculateItem>
+                Log.d("CalculateActivity", "Picks found: " + calculateItems.size)
+
+        calculateAdapter = CalculateAdapter(calculateItems,this)
+        calculate_rv.adapter = calculateAdapter
+        calculateAdapter.notifyDataSetChanged()
+        calculate_btn.setOnClickListener {
+            var investmentVal = calculate_et.text.toString()
+            if(isNumber(investmentVal)){
+                updateListWithNewCapital(investmentVal)
+            }
+            else{
+                Toast.makeText(this,"Not a valid number",Toast.LENGTH_LONG).show()
+            }
+        }
 
     }
     catch (e : Exception){
         e.printStackTrace()
     }
 
-            calculate_btn = findViewById(R.id.calculate_button)
-            calculate_rv = findViewById(R.id.calculate_list_rv)
-            calculate_et = findViewById(R.id.calculate_invest_et)
-            calculate_btn.setOnClickListener {
-                var investmentVal = calculate_et.text.toString()
-                if(isNumber(investmentVal)){
-
-                }
-                else{
-                    Toast.makeText(this,"Not a valid number",Toast.LENGTH_LONG).show()
-                }
-            }
 
         }
 
@@ -51,5 +61,12 @@ class CalculateActivity : Activity() {
 
     fun isNumber(s: String?): Boolean {
         return if (s.isNullOrEmpty()) false else s.all { Character.isDigit(it) }
+    }
+
+    fun updateListWithNewCapital(investCap :String){
+        calculateItems.forEach({
+            it.totalCapital = investCap as Float
+        })
+        calculateAdapter.notifyDataSetChanged()
     }
 }
