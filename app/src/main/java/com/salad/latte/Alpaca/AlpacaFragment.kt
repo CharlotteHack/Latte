abc272c5d8fc7dfe5640a6317a197253568a5ef1
mohamedsalad <kukgiplayer2@gmail.com>
2022-05-21 22:26:50 -0400

    ok

---
+++ app/src/main/java/com/salad/latte/Alpaca/AlpacaFragment.kt
@@ -0,0 +1,6 @@
+package com.salad.latte.Alpaca
+
+class AlpacaFragment
+{
+
+}
---
+++ app/src/main/java/com/salad/latte/Alpaca/AlpacaUser.kt
@@ -0,0 +1,10 @@
+package com.salad.latte.Alpaca
+
+class AlpacaUser(name :String) {
+    var name = ""
+
+
+    private fun getAlpacaName() : String{
+        return name
+    }
+}
