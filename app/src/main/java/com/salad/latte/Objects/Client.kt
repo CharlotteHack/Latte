008b7107dd287fbd0d695043a741ab330303109d
mohamedsalad <kukgiplayer2@gmail.com>
2022-09-10 12:02:06 -0400

    Updates

---
+++ app/src/main/java/com/salad/latte/Objects/Client.kt
@@ -0,0 +1,14 @@
+package com.salad.latte.Objects
+
+class Client {
+    var client_account_number = ""
+     var client_name = ""
+     var client_realized_profit = ""
+     var client_unrealized_profit = ""
+     var client_email = ""
+     var client_balance = ""
+
+    init {
+
+    }
+}
