aeb8494bc17f85211abd35d90c275756bc3ceed4
mohamedsalad <kukgiplayer2@gmail.com>
2022-10-27 23:34:56 -0400

    Added more logic for depositing on the settings tab

---
+++ app/src/main/java/com/salad/latte/ClientManagement/ActivityWithdrawalSuccessful.kt
@@ -0,0 +1,20 @@
+package com.salad.latte.ClientManagement
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.salad.latte.R
+
+class ActivityWithdrawalSuccessful : AppCompatActivity() {
+
+    lateinit var successBtn : Button
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deposit_success)
+        successBtn = findViewById<Button>(R.id.deposit_success_btn)
+        successBtn.setOnClickListener {
+            finish()
+        }
+    }}
